36352f6846b1e4f4273b8852ef476494ff086d31
lucasjdc <lucasjpdc@gmail.com>
2025-05-31 00:20:33 -0300

    Separando em parágrafos

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    verificaPalavrasDuplicadas(texto);
+    quebraEmParagrafos(texto);
 });
+
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.map((paragrafo) =>{
+        return verificaPalavrasDuplicadas(paragrafo);
+    });
+    console.log(contagem);
+}
 
 function verificaPalavrasDuplicadas(texto) {
     const listaPalavras = texto.split(' ');
@@ -13,5 +21,5 @@ function verificaPalavrasDuplicadas(texto) {
     listaPalavras.forEach(palavra => {
         resultado[palavra] = (resultado[palavra] || 0) + 1
     });
-    console.log(resultado);
+    return resultado;
 }
