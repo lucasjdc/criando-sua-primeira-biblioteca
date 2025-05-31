fc389ef4e38ad85078511134b1e276944859e2be
lucasjdc <lucasjpdc@gmail.com>
2025-05-31 11:20:53 -0300

    Organizando a saída dos dados

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 'use strict';
+const { performance } = require('perf_hooks');
 const fs = require('fs');
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
+const inicio = performance.now();
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
     quebraEmParagrafos(texto);
+
+    const fim = performance.now();
+    console.log(`Tempo de execução: ${(fim - inicio).toFixed(4)} ms`);
 });
 
 function quebraEmParagrafos(texto) {
     const paragrafos = texto.toLowerCase().split('\n');
-    const contagem = paragrafos.map((paragrafo) =>{
+    const contagem = paragrafos.flatMap((paragrafo) => {
+        if (!paragrafo) return [];
         return verificaPalavrasDuplicadas(paragrafo);
     });
     console.log(contagem);
